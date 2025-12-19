2edb0bb0efa412f410b324c0bd5645d482b76f76
JARD Noan <noan.pujol@gmail.com>
2025-12-19 21:35:09 +0100

    bg preview component edited

--- src/components/PreviewFile.tsx
+++ src/components/PreviewFile.tsx
@@ -34,7 +34,7 @@ export function PreviewFile({ files }: PreviewFileProps) {
             }
             else {
                 gridItems.push(
-                    <div key={i} className={`bg-gray-200 w-full gap-4 ${reach === 2 ? 'h-25 xl:h-40' : 'h-25 xl:h-40'} border rounded-lg flex flex-col items-center justify-center`}>
+                    <div key={i} className={`bg-gray-200/40 w-full gap-4 ${reach === 2 ? 'h-25 xl:h-40' : 'h-25 xl:h-40'} rounded-lg flex flex-col items-center justify-center`}>
                         <img src="/fileicon.png"
                             className="w-4 h-5 xl:w-7 xl:h-9" />
                         <p className="text-black text-center truncate w-9/12 text-xs xl:text-base">{file.name}</p>
